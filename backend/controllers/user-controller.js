3358a90e642a7d79d015d9426df0ef475ebcd13e
Sharma-ayush-20 <sharmaayush201104@gmail.com>
2025-07-09 19:46:59 +0530

    made login and logout controller and routes

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -4,26 +4,26 @@ import bcrypt from 'bcryptjs'
 
 const signUp = async (req, res) => {
     try {
-        
-        const {fullname, email, password, confirmPassword} = req.body;
 
-        if (!fullname || !email || !password || !confirmPassword){
+        const { fullname, email, password, confirmPassword } = req.body;
+
+        if (!fullname || !email || !password || !confirmPassword) {
             return res.status(400).json({
                 success: false,
                 message: "Please provide all details",
             })
         }
 
-        if(password !== confirmPassword){
+        if (password !== confirmPassword) {
             return res.status(400).json({
                 success: false,
                 message: "Password do not match"
             })
         }
 
-        const user = await User.findOne({email})
+        const user = await User.findOne({ email })
 
-        if(user){
+        if (user) {
             return res.status(400).json({
                 success: false,
                 message: "User is already existed"
@@ -35,7 +35,7 @@ const signUp = async (req, res) => {
         let hashPassword = await bcrypt.hash(password, salt)
 
         const newUser = await User({
-            fullname,email,password : hashPassword
+            fullname, email, password: hashPassword
         })
 
         await newUser.save();
@@ -54,11 +54,73 @@ const signUp = async (req, res) => {
             success: false,
             message: error.message,
         })
+    }
+}
 
+const login = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ success: false, message: "Email and password is required" })
+        }
+
+        const existedUser = await User.findOne({ email })
+
+        if (!existedUser) {
+            return res.status(400).json({ success: false, message: "email not existed. plz signup" })
+        }
+
+        //check password is correct or not
+        const matchPassword = await bcrypt.compare(password, existedUser.password)
+
+        if (!matchPassword) {
+            return res.status(400).json({ success: false, message: "password is incorrect" })
+        }
+
+        createTokenAndSaveCookie(existedUser._id, res)
+
+        return res.status(200).json({
+            success: true, message: "User Login SuccessFully",
+            existedUser: {
+                _id: existedUser._id,
+                fullname: existedUser.fullname,
+                email: existedUser.email,
+            }
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        })
+    }
+}
+
+const logOut = async (req, res) => {
+    try {
+
+        res.clearCookie("jwt")
+
+        return res.status(200).json({
+            success: true,
+            message: "Logged out successfully",
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        })
     }
 }
 
 export {
-    signUp,       
+    signUp,
+    login,
+    logOut,
 }
 
--- backend/routes/user-route.js
+++ backend/routes/user-route.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { signUp } from '../controllers/user-controller.js'
+import { login, logOut, signUp } from '../controllers/user-controller.js'
 const userRouter = express.Router()
 
 userRouter.post("/signup", signUp)
+userRouter.post("/login", login)
+userRouter.post("/logout", logOut)
 
 export default userRouter
